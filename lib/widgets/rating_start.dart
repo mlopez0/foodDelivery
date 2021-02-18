2b7dc8b5d8fa9abf0ab73a87896d4ac821e8f730
marvin <marvinrenelopez@gmail.com>
2021-02-18 07:42:23 -0600

    Initial Commit

---
+++ lib/widgets/rating_start.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RatingStarts extends StatelessWidget {
+  final int rating;
+  RatingStarts(this.rating);
+
+  @override
+  Widget build(BuildContext context) {
+    String starts = '';
+    for (int i = 0; i < rating; i++) {
+      starts += 'X  ';
+    }
+    starts.trim();
+    return Text(
+      starts,
+      style: TextStyle(
+        fontSize: 18.0,
+      ),
+    );
+  }
+}
